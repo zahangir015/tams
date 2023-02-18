2dded73278bd5518b4b3a652cffecbe842c54907
zahangir015 <zahangirahammad@gmail.com>
2023-02-18 10:56:58 +0600

    migration issue resolve

--- modules/account/migrations/m230126_174311_create_chart_of_account_table.php
+++ modules/account/migrations/m230126_174311_create_chart_of_account_table.php
@@ -5,7 +5,7 @@ use yii\db\Migration;
 /**
  * Handles the creation of table `{{%chart_of_account}}`.
  */
-class m230126_174211_create_chart_of_account_table extends Migration
+class m230126_174311_create_chart_of_account_table extends Migration
 {
     /**
      * {@inheritdoc}
