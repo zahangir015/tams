9591c82b000618e851ee164ef7a4f02b38527bd8
zahangir015 <zahangirahammad@gmail.com>
2023-03-22 23:08:00 +0600

    Latest update

--- modules/hrm/views/employee-payroll/index.php
+++ modules/hrm/views/employee-payroll/index.php
@@ -1,11 +1,15 @@
 <?php
 
+use app\components\Utilities;
+use app\components\WidgetHelper;
 use app\modules\hrm\models\EmployeePayroll;
+use kartik\select2\Select2;
 use yii\helpers\Html;
 use yii\helpers\Url;
-use yii\grid\ActionColumn;
-use yii\grid\GridView;
+use kartik\grid\ActionColumn;
+use kartik\grid\GridView;
 use yii\widgets\Pjax;
+
 /** @var yii\web\View $this */
 /** @var app\modules\hrm\models\search\EmployeePayrollSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
@@ -14,28 +18,21 @@ $this->title = Yii::t('app', 'Employee Payrolls');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="employee-payroll-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a(Yii::t('app', 'Create Employee Payroll'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php Pjax::begin(); ?>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'uid',
-            'employeeId',
+            ['class' => 'kartik\grid\SerialColumn'],
+            [
+                'attribute' => 'employeeId',
+                'value' => function ($model) {
+                    return $model->employee->firstName . '' . $model->employee->lastName;
+                },
+                'filter' => Select2::widget(WidgetHelper::ajaxSelect2Widget('employeeId', '/hrm/employee/get-employees', false, 'employeeId', 'employeeId'))
+            ],
             'gross',
             'tax',
-            //'paymentMode',
+            'paymentMode',
             //'remarks',
             //'status',
             //'createdBy',
@@ -43,14 +40,41 @@ $this->params['breadcrumbs'][] = $this->title;
             //'updatedBy',
             //'updatedAt',
             [
-                'class' => ActionColumn::className(),
+                'class' => ActionColumn::class,
                 'urlCreator' => function ($action, EmployeePayroll $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                 }
+                    return Url::toRoute([$action, 'uid' => $model->uid]);
+                },
+                'width' => '150px',
+                'template' => '{view} {edit} {delete}',
+                'viewOptions' => ['role' => 'modal-remote', 'title' => 'View', 'data-toggle' => 'tooltip'],
+                'buttons' => Utilities::getBasicActionColumnArray()
             ],
         ],
+        'toolbar' => [
+            [
+                'content' =>
+                    Html::a('<i class="fas fa-plus"></i>', ['/hrm/employee-payroll/create'], [
+                        'title' => Yii::t('app', 'Add Employee Payroll'),
+                        'class' => 'btn btn-success'
+                    ]) . ' ' .
+                    Html::a('<i class="fas fa-redo"></i>', ['/hrm/employee-payroll/index'], [
+                        'class' => 'btn btn-primary',
+                        'title' => Yii::t('app', 'Reset Grid')
+                    ]),
+            ],
+            '{export}',
+            '{toggleData}'
+        ],
+        'pjax' => true,
+        'bordered' => true,
+        'striped' => false,
+        'condensed' => false,
+        'responsive' => true,
+        'hover' => true,
+        'panel' => [
+            'heading' => '<i class="fas fa-list-alt"></i> ' . Html::encode($this->title),
+            'type' => GridView::TYPE_DARK
+        ],
     ]); ?>
-
-    <?php Pjax::end(); ?>
 
 </div>
