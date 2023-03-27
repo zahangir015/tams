8ca2fe62c6c684ef938eab7d5396969e28cd098a
zahangir015 <zahangirahammad@gmail.com>
2023-03-27 08:14:50 +0600

    Latest update

---
+++ modules/hrm/migrations/m230326_044043_create_plan_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%plan}}`.
+ */
+class m230326_044043_create_plan_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%plan}}', [
+            'id' => $this->primaryKey(),
+            'uid' => $this->string(36)->notNull()->unique(),
+            'name' => $this->string()->notNull()->unique(),
+            'userLimit' => $this->string()->notNull(),
+            'monthlySubscriptionFee' => $this->double()->notNull(),
+            'yearlySubscriptionFee' => $this->double()->notNull(),
+            'modules' => $this->text(),
+            'status' => $this->boolean()->defaultValue(1),
+            'createdBy' => $this->integer(11)->notNull(),
+            'createdAt' => $this->integer(11)->notNull(),
+            'updatedBy' => $this->integer(11)->null(),
+            'updatedAt' => $this->integer(11)->null(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%plan}}');
+    }
+}
