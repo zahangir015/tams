794667c51645f10d9b731702ce89e8dca512d542
zahangir015 <zahangirahammad@gmail.com>
2023-04-17 07:54:24 +0000

    Conflict issue resolved

--- modules/sale/models/search/CustomerSearch.php
+++ modules/sale/models/search/CustomerSearch.php
@@ -46,12 +46,8 @@ class CustomerSearch extends Customer
 
         // add conditions that should always apply here
         $query->where([self::tableName() . '.status' => GlobalConstant::ACTIVE_STATUS])
-<<<<<<< HEAD
-            ->andWhere([self::tableName() . '.agencyId' => Yii::$app->user->identity->agencyId]);
-=======
             ->andWhere([self::tableName().'.agencyId' => Yii::$app->user->identity->agencyId]);
->>>>>>> 3cc7506 (Latest update)
-
+	
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
             'sort'=> ['defaultOrder' => ['company' => SORT_ASC]],
