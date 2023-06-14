f9d3346511718e80e510732619f49f86de1dc74a
zahangir015 <zahangirahammad@gmail.com>
2023-06-14 18:51:16 +0600

    latest update

--- modules/admin/views/user/login.php
+++ modules/admin/views/user/login.php
@@ -10,63 +10,137 @@ use yii\bootstrap4\ActiveForm;
 $this->title = Yii::t('rbac-admin', 'Login');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="card">
+
+<?php $form = ActiveForm::begin(['id' => 'login-form']) ?>
+
+<div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+    <h5 class="lead text-center">Sign in with</h5>
+</div>
+
+<?= $form->field($model, 'username', [
+    'options' => ['class' => 'form-group has-feedback'],
+    'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-envelope"></span></div></div>',
+    'template' => '{beginWrapper}{input}{error}{endWrapper}',
+    'wrapperOptions' => ['class' => 'input-group mb-3']
+])->label(false)->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
+
+<?= $form->field($model, 'password', [
+    'options' => ['class' => 'form-group has-feedback'],
+    'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-lock"></span></div></div>',
+    'template' => '{beginWrapper}{input}{error}{endWrapper}',
+    'wrapperOptions' => ['class' => 'input-group mb-3']
+])->label(false)->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
+
+<div class="row">
+    <div class="col-6">
+        <?= $form->field($model, 'rememberMe')->checkbox([
+            'template' => '<div class="icheck-success">{input}{label}</div>',
+            'labelOptions' => [
+                'class' => ''
+            ],
+            'uncheck' => null
+        ]) ?>
+    </div>
+    <div class="col-6">
+        <?= Html::a('<i class="fa fa-key mr-2"></i> Forgot Password', ['user/request-password-reset'], ['class' => 'text-dark']) ?>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-12">
+        <?= Html::submitButton('Sign In', ['class' => 'btn btn-success btn-block', 'name' => 'login-button']) ?>
+    </div>
+</div>
+
+
+<?php ActiveForm::end(); ?>
+
+<!-- Email input -->
+<!--<div class="form-outline mb-4">
+    <input type="email" id="form3Example3" class="form-control form-control-lg"
+           placeholder="Enter a valid email address"/>
+    <label class="form-label" for="form3Example3">Email address</label>
+</div>-->
+
+<!-- Password input -->
+<!--<div class="form-outline mb-3">
+    <input type="password" id="form3Example4" class="form-control form-control-lg"
+           placeholder="Enter password"/>
+    <label class="form-label" for="form3Example4">Password</label>
+</div>-->
+
+<!--<div class="d-flex justify-content-between align-items-center">
+    <div class="form-check mb-0">
+        <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3"/>
+        <label class="form-check-label" for="form2Example3">
+            Remember me
+        </label>
+    </div>
+    <a href="#!" class="text-body">Forgot password?</a>
+</div>-->
+
+<!--<div class="text-center text-lg-start mt-4 pt-2">
+    <button type="button" class="btn btn-success btn-lg"
+            style="padding-left: 2.5rem; padding-right: 2.5rem;">Login
+    </button>
+    <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!" class="link-danger">Register</a></p>
+</div>-->
+<!--<div class="card">
     <div class="card-body login-card-body shadow rounded-lg">
 
-        <?php $form = ActiveForm::begin(['id' => 'login-form']) ?>
 
-        <?= $form->field($model, 'username', [
+
+        <?php /*= $form->field($model, 'username', [
             'options' => ['class' => 'form-group has-feedback'],
             'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-envelope"></span></div></div>',
             'template' => '{beginWrapper}{input}{error}{endWrapper}',
             'wrapperOptions' => ['class' => 'input-group mb-3']
-        ])->label(false)->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
+        ])->label(false)->textInput(['placeholder' => $model->getAttributeLabel('username')]) */ ?>
 
-        <?= $form->field($model, 'password', [
+        <?php /*= $form->field($model, 'password', [
             'options' => ['class' => 'form-group has-feedback'],
             'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-lock"></span></div></div>',
             'template' => '{beginWrapper}{input}{error}{endWrapper}',
             'wrapperOptions' => ['class' => 'input-group mb-3']
-        ])->label(false)->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
+        ])->label(false)->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) */ ?>
 
         <div class="row">
             <div class="col-6">
-                <?= $form->field($model, 'rememberMe')->checkbox([
+                <?php /*= $form->field($model, 'rememberMe')->checkbox([
                     'template' => '<div class="icheck-success">{input}{label}</div>',
                     'labelOptions' => [
                         'class' => ''
                     ],
                     'uncheck' => null
-                ]) ?>
+                ]) */ ?>
             </div>
             <div class="col-6">
-                <?= Html::a('<i class="fa fa-key mr-2"></i> Forgot Password', ['user/request-password-reset'], ['class' => 'text-dark']) ?>
+                <?php /*= Html::a('<i class="fa fa-key mr-2"></i> Forgot Password', ['user/request-password-reset'], ['class' => 'text-dark']) */ ?>
             </div>
         </div>
 
         <div class="row">
             <div class="col-12">
-                <?= Html::submitButton('Sign In', ['class' => 'btn btn-success btn-block', 'name' => 'login-button']) ?>
+                <?php /*= Html::submitButton('Sign In', ['class' => 'btn btn-success btn-block', 'name' => 'login-button']) */ ?>
             </div>
         </div>
 
 
-        <?php ActiveForm::end(); ?>
+        <?php /*ActiveForm::end(); */ ?>
 
         <div class="social-auth-links text-center mb-3">
             <p class="text-center">- OR -</p>
             <div class="row  mt-2">
                 <div class="col-12 mb-2">
-                    <?= Html::a('Register a new membership', ['/admin/user/account'], ['class' => 'btn btn-success btn-block']) ?>
+                    <?php /*= Html::a('Register a new membership', ['/admin/user/account'], ['class' => 'btn btn-success btn-block']) */ ?>
                 </div>
                 <div class="col-12">
-                    <?= Html::a('Inquire', ['/support/inquiry/'], ['class' => 'btn btn-success btn-block']) ?>
+                    <?php /*= Html::a('Inquire', ['/support/inquiry/'], ['class' => 'btn btn-success btn-block']) */ ?>
                 </div>
             </div>
         </div>
     </div>
-    <!-- /.login-card-body -->
-</div>
+</div>-->
 <!--<div class="site-login">
     <h1><? /*= Html::encode($this->title) */ ?></h1>
 
--- views/layouts/main-login.php
+++ views/layouts/main-login.php
@@ -9,29 +9,79 @@ $this->registerCssFile('https://code.ionicframework.com/ionicons/2.0.1/css/ionic
 \hail812\adminlte3\assets\PluginAsset::register($this)->add(['fontawesome', 'icheck-bootstrap']);
 ?>
 <?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>MYTRAMS | Log in</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php $this->registerCsrfMetaTags() ?>
-    <?php $this->head() ?>
-</head>
-<body class="hold-transition login-page">
-<?php  $this->beginBody() ?>
-<div class="login-box rounded-lg shadow">
-    <div class="login-logo">
-        <a href="<?=Yii::$app->homeUrl?>"><b>MYTRAMS</b></a>
-    </div>
-    <!-- /.login-logo -->
-    <?= $content ?>
-</div>
-<!-- /.login-box -->
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>MYTRAMS | Log in</title>
+        <!-- Tell the browser to be responsive to screen width -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php $this->registerCsrfMetaTags() ?>
+        <?php $this->head() ?>
+        <style>
+            .divider:after,
+            .divider:before {
+                content: "";
+                flex: 1;
+                height: 1px;
+                background: #eee;
+            }
+            .h-custom {
+                height: calc(100% - 73px);
+            }
+            @media (max-width: 450px) {
+                .h-custom {
+                    height: 100%;
+                }
+            }
+        </style>
+    </head>
+    <body>
+    <?php $this->beginBody() ?>
+    <section class="vh-100">
+        <div class="container-fluid h-custom">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-md-9 col-lg-6 col-xl-5">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                         class="img-fluid" alt="Sample image">
+                </div>
+                <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                    <?= $content ?>
+                </div>
+            </div>
+        </div>
+        <!--<div class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
 
-<?php $this->endBody() ?>
-</body>
-</html>
+            <div class="text-white mb-3 mb-md-0">
+                Copyright © <?php /*= date('Y') */?>. All rights reserved.
+            </div>
+            <div>
+                <a href="#!" class="text-white me-4">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#!" class="text-white me-4">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#!" class="text-white me-4">
+                    <i class="fab fa-google"></i>
+                </a>
+                <a href="#!" class="text-white">
+                    <i class="fab fa-linkedin-in"></i>
+                </a>
+            </div>
+        </div>-->
+    </section>
+    <!--<div class="login-box rounded-lg shadow">
+    <div class="login-logo">
+        <a href="<?php /*=Yii::$app->homeUrl*/ ?>"><b>MYTRAMS</b></a>
+    </div>-->
+    <!-- /.login-logo -->
+    <?php /*= $content */ ?><!--
+</div>-->
+    <!-- /.login-box -->
+
+    <?php $this->endBody() ?>
+    </body>
+    </html>
 <?php $this->endPage() ?>
