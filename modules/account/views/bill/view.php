d4fea2d4268b48545effce17ea95904fbe6bdb25
zahangir015 <zahangirahammad@gmail.com>
2023-08-21 12:09:52 +0600

    Bill view issue resolved

--- modules/account/views/bill/view.php
+++ modules/account/views/bill/view.php
@@ -14,19 +14,6 @@ $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="bill-view">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
     <div class="row">
         <div class="col-9">
             <div class="card">
