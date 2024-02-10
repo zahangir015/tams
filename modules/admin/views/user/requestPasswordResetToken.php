7b5c636572373498099f2c72d616eef1365516e6
Md. Nahid Ullah Joy <69644252+mdjoy555@users.noreply.github.com>
2024-02-10 11:41:04 +0600

    Update requestPasswordResetToken.php

--- modules/admin/views/user/requestPasswordResetToken.php
+++ modules/admin/views/user/requestPasswordResetToken.php
@@ -12,14 +12,14 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="card">
     <div class="card-body login-card-body shadow rounded-lg">
-        <h3><?= Html::encode($this->title) ?></h3>
+        <h3 class="text-center" style="color: #337abe;"><?= Html::encode($this->title) ?></h3>
         <p>Please fill out your email. A link to reset password will be sent there.</p>
         <div class="row">
             <div class="col-lg">
                 <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form','options' => ['class' => 'form-group has-feedback'],]); ?>
-                <?= $form->field($model, 'email') ?>
+                <?= $form->field($model, 'email')->textInput(['maxlength' => true, 'type' => 'email', 'placeholder' => $model->getAttributeLabel('email'), 'style' => 'background-color: #D6EADF; border: none;'])->label(false) ?>
                 <div class="form-group">
-                    <?= Html::submitButton(Yii::t('rbac-admin', 'Send'), ['class' => 'btn btn-success btn-block']) ?>
+                    <?= Html::submitButton(Yii::t('rbac-admin', 'Send'), ['class' => 'btn btn-success btn-block', 'style' => 'background-color: #337abe; border-radius: 10px;']) ?>
                 </div>
                 <?php ActiveForm::end(); ?>
             </div>
@@ -28,10 +28,10 @@ $this->params['breadcrumbs'][] = $this->title;
             <p class="text-center">- OR -</p>
             <div class="row  mt-2">
                 <div class="col-12 mb-2">
-                    <?= Html::a('Sign In', ['/admin/user/login'], ['class' => 'btn btn-success btn-block']) ?>
+                    <?= Html::a('Login', ['/admin/user/login'], ['class' => 'btn btn-success btn-block', 'style' => 'background-color: #337abe; border-radius: 10px;']) ?>
                 </div>
                 <div class="col-12">
-                    <?= Html::a('Inquire', ['/support/inquiry/'], ['class' => 'btn btn-success btn-block']) ?>
+                    <?= Html::a('Inquire', ['/admin/user/inquiry'], ['class' => 'btn btn-success btn-block', 'style' => 'background-color: #337abe; border-radius: 10px;']) ?>
                 </div>
             </div>
         </div>
