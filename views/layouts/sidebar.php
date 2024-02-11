cfffb0062252f0222af8c92638c3bde9da2924bf
Md. Nahid Ullah Joy <69644252+mdjoy555@users.noreply.github.com>
2024-02-11 16:19:07 +0600

    Update sidebar.php

--- views/layouts/sidebar.php
+++ views/layouts/sidebar.php
@@ -688,21 +688,6 @@ $items = [
                         sidebarMenu[j].classList.remove("menu-open");
                     }
                     sidebarMenu[j].children[1].style.display = "none";
-                    for(let k=0;k<sidebarMenu[i].children[1].children.length;k++)
-                    {
-                        if(sidebarMenu[i].children[1].children[k].classList.contains("has-treeview"))
-                        {
-                            if(sidebarMenu[i].children[1].children[k].classList.contains("menu-is-opening"))
-                            {
-                                sidebarMenu[i].children[1].children[k].classList.remove("menu-is-opening");
-                            }
-                            if(sidebarMenu[i].children[1].children[k].classList.contains("menu-open"))
-                            {
-                                sidebarMenu[i].children[1].children[k].classList.remove("menu-open");
-                            }
-                            sidebarMenu[i].children[1].children[k].children[1].style.display = "none";
-                        }
-                    }
                 }
             }
             for(let j=0;j<sidebarMenu[i].children[1].children.length;j++)
