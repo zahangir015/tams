0524f8f41b4ad5d6b3c46a723475f6e98bd04c3c
Md. Nahid Ullah Joy <69644252+mdjoy555@users.noreply.github.com>
2024-02-22 15:12:53 +0600

    Add files via upload

---
+++ modules/serviceInventory/views/hotelInventory/index.php
@@ -0,0 +1,57 @@
+<?php
+
+use app\modules\serviceInventory\models\HotelInventory;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/** @var yii\web\View $this */
+/** @var app\modules\serviceInventory\models\HotelInventorySearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = Yii::t('app', 'Hotel Inventories');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="hotel-inventory-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Hotel Inventory'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin(); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'uid',
+            'supplierId',
+            'hotelName',
+            'hotelAddress',
+            //'countryId',
+            //'cityId',
+            //'hotelCategoryId',
+            //'status',
+            //'createdAt',
+            //'createdBy',
+            //'updatedAt',
+            //'updatedBy',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, HotelInventory $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
