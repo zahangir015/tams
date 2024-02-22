77ab27be8b41d94f386a4eb7f485b19634d0d566
Md. Nahid Ullah Joy <69644252+mdjoy555@users.noreply.github.com>
2024-02-22 15:00:38 +0600

    Update Module.php

--- modules/serviceInventory/Module.php
+++ modules/serviceInventory/Module.php
@@ -1,1 +1,24 @@
+<?php
 
+namespace app\modules\serviceInventory;
+
+/**
+ * serviceInventory module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'app\modules\serviceInventory\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
