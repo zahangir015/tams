e8fb19f48bc41ff5582cd13961d15ec7e589268c
Md. Nahid Ullah Joy <69644252+mdjoy555@users.noreply.github.com>
2024-02-22 15:02:49 +0600

    Update AmenityController.php

--- modules/serviceInventory/controllers/AmenityController.php
+++ modules/serviceInventory/controllers/AmenityController.php
@@ -1,1 +1,150 @@
+<?php
 
+namespace app\modules\serviceInventory\controllers;
+
+use Yii;
+use app\components\GlobalConstant;
+use app\components\Utilities;
+use app\modules\serviceInventory\models\Amenity;
+use app\modules\serviceInventory\models\AmenitySearch;
+use app\controllers\ParentController;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\web\Response;
+
+/**
+ * AmenityController implements the CRUD actions for Amenity model.
+ */
+class AmenityController extends ParentController
+{
+    /**
+     * @inheritDoc
+     */
+    public function behaviors()
+    {
+        return array_merge(
+            parent::behaviors(),
+            [
+                'verbs' => [
+                    'class' => VerbFilter::className(),
+                    'actions' => [
+                        'delete' => ['POST'],
+                    ],
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Lists all Amenity models.
+     *
+     * @return string
+     */
+    public function actionIndex(): string
+    {
+        $searchModel = new AmenitySearch();
+        $dataProvider = $searchModel->search($this->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Amenity model.
+     * @param string $uid UID
+     * @return string
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView(string $uid): string
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($uid),
+        ]);
+    }
+
+    /**
+     * Creates a new Amenity model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return string|\yii\web\Response
+     */
+    public function actionCreate()
+    {
+        $model = new Amenity();
+
+        if ($this->request->isPost) {
+            if ($model->load($this->request->post()) && $model->save()) {
+                Yii::$app->session->setFlash('success','Amenity created successfully');
+                return $this->redirect(['view', 'id' => $model->id]);
+            }
+            Yii::$app->session->setFlash('danger',Utilities::processErrorMessages($model->getErrors()));
+        } else {
+            $model->loadDefaultValues();
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Updates an existing Amenity model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param string $uid UID
+     * @return string|\yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate(string $uid)
+    {
+        $model = $this->findModel($uid);
+
+        if ($this->request->isPost)
+        {
+            if($model->load($this->request->post()) && $model->save())
+            {
+                Yii::$app->session->setFlash('success','Amenity updated successfully');
+                return $this->redirect(['view', 'id' => $model->id]);
+            }
+            Yii::$app->session->setFlash('danger',Utilities::processErrorMessages($model->getErrors()));
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Deletes an existing Amenity model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param string $uid UID
+     * @return \yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete(string $uid): Response
+    {
+        $model = $this->findModel($uid);
+        $model->status = GlobalConstant::INACTIVE_STATUS;
+        $model->save();
+        Yii::$app->session->setFlash('success','Amenity deleted successfully');
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Amenity model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param string $uid UID
+     * @return Amenity the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel(string $uid): Amenity
+    {
+        if (($model = Amenity::findOne(['uid' => $uid])) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
+    }
+}
