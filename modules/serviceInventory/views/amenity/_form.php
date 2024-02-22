265d2951378210e1c75ab7abe5aa7769308e7b54
Md. Nahid Ullah Joy <69644252+mdjoy555@users.noreply.github.com>
2024-02-22 15:11:13 +0600

    Update _form.php

--- modules/serviceInventory/views/amenity/_form.php
+++ modules/serviceInventory/views/amenity/_form.php
@@ -1,1 +1,36 @@
+<?php
 
+use app\components\GlobalConstant;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\modules\serviceInventory\models\Amenity $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="amenity-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'uid')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'status')->textInput() ?>
+
+    <?= $form->field($model, 'createdAt')->textInput() ?>
+
+    <?= $form->field($model, 'createdBy')->textInput() ?>
+
+    <?= $form->field($model, 'updatedAt')->textInput() ?>
+
+    <?= $form->field($model, 'updatedBy')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
