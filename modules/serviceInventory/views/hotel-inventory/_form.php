74f81c929c000a4d62268d191fab6489bb541e22
Md. Nahid Ullah Joy <69644252+mdjoy555@users.noreply.github.com>
2024-02-27 13:14:30 +0600

    Add files via upload

---
+++ modules/serviceInventory/views/hotel-inventory/_form.php
@@ -0,0 +1,46 @@
+<?php
+
+use app\components\GlobalConstant;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\modules\serviceInventory\models\HotelInventory $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="hotel-inventory-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'uid')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'supplierId')->textInput() ?>
+
+    <?= $form->field($model, 'hotelName')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'hotelAddress')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'countryId')->textInput() ?>
+
+    <?= $form->field($model, 'cityId')->textInput() ?>
+
+    <?= $form->field($model, 'hotelCategoryId')->textInput() ?>
+
+    <?= $form->field($model, 'status')->textInput() ?>
+
+    <?= $form->field($model, 'createdAt')->textInput() ?>
+
+    <?= $form->field($model, 'createdBy')->textInput() ?>
+
+    <?= $form->field($model, 'updatedAt')->textInput() ?>
+
+    <?= $form->field($model, 'updatedBy')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ modules/serviceInventory/views/hotel-inventory/update.php
@@ -0,0 +1,23 @@
+<?php
+
+use yii\helpers\Html;
+
+/** @var yii\web\View $this */
+/** @var app\modules\serviceInventory\models\HotelInventory $model */
+
+$this->title = Yii::t('app', 'Update Hotel Inventory: {name}', [
+    'name' => $model->name,
+]);
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Hotel Inventories'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'uid' => $model->uid]];
+$this->params['breadcrumbs'][] = Yii::t('app', 'Update');
+?>
+<div class="hotel-inventory-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
