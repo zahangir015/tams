f21b7a385d51e881448c0103c0eada16ed7f44ea
Md. Nahid Ullah Joy <69644252+mdjoy555@users.noreply.github.com>
2024-03-28 14:57:05 +0600

    Update create.php

--- modules/serviceInventory/views/hotel-inventory/create.php
+++ modules/serviceInventory/views/hotel-inventory/create.php
@@ -4,17 +4,22 @@ use yii\helpers\Html;
 
 /** @var yii\web\View $this */
 /** @var app\modules\serviceInventory\models\HotelInventory $model */
+/** @var app\moduels\serviceInventory\models\HotelInventoryRoomDetail $model*/
+/** @var app\moduels\serviceInventory\models\HotelInventoryAmenity $model*/
 
 $this->title = Yii::t('app', 'Create Hotel Inventory');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Hotel Inventories'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="hotel-inventory-create">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
     <?= $this->render('_form', [
         'model' => $model,
+        'model2' => $model2,
+        'model3' => $model3,
+        'model4' => $model4,
+        'roomDetail' => $roomDetail,
+        'categories' => $categories,
+        'roomTypes' => $roomTypes
     ]) ?>
 
 </div>
